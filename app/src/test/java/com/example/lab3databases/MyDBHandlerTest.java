4ae7598bf62ce169e5a6f6d266c240cce5dda244
samith <samithfernando539@gmail.com>
2025-11-10 19:31:02 -0500

    create unit test file

---
+++ app/src/test/java/com/example/lab3databases/MyDBHandlerTest.java
@@ -0,0 +1,15 @@
+package com.example.lab3databases;
+
+import junit.framework.TestCase;
+
+public class MyDBHandlerTest extends TestCase {
+
+    public void testAddProduct() {
+    }
+
+    public void testFindProduct() {
+    }
+
+    public void testDeleteProduct() {
+    }
+}
